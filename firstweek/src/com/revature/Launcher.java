e9e99860e8ec098768f4539df02469dbee992810
Itskiven <itskiven1996@gmail.com>
2022-03-15 10:37:04 -0400

    Challenge 1 completed

--- firstweek/src/com/revature/Launcher.java
+++ firstweek/src/com/revature/Launcher.java
@@ -1,10 +1,52 @@
 package com.revature;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Launcher {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner (System.in);
+		LinkedList<Integer> listA = new LinkedList<>();
+		LinkedList<Integer> listB = new LinkedList<>();
+		int answer = 0;
+		
+		System.out.println("Size of LinkedList A: ");
+		int m = scan.nextInt ();
+		scan.nextLine();
+		System.out.println("Values for LinkedList A: ");
+		for (int i = 0; i<m; i++) {
+			int a = scan.nextInt ();
+			listA.add(a);
+		}
+		
+		System.out.println("Size of LinkedList B: ");
+		int n = scan.nextInt ();
+		scan.nextLine();
+		System.out.println("Values for LinkedList A: ");
+		for (int i = 0; i<m; i++) {
+			int a = scan.nextInt ();
+			listB.add(a);
+		}
+		
+		outerloop:
+		for (int i = 0; i<Math.max(m, n); i++) {
+			for (int j = 0; j<Math.min(m, n); j++) {
+				if (listA.get(i) == listB.get(j)) {
+					answer = listA.get(i);
+					System.out.println(answer);
+					break outerloop;
+					
+				}
+			}
+		}
+		
+		
 	}
+	
+	
 
 }
